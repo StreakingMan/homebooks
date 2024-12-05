0352bf4e7d01af9ae287120848f45fd09c28c4b1
streakingman <z_max_y@163.com>
2024-12-05 14:25:18 +0800

    chore: update README.md

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  devServer: {
+    port: 4000,
+  },
   modules: ['@nuxt/ui', '@nuxt/eslint'],
 
   eslint: {
